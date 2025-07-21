754b06f7340a2e9c185835da11ea01fb5acb40ef
Tsche <che@pydong.org>
2025-07-21 04:32:15 +0000

    update dependencies, reorganize src/

--- conanfile.py
+++ conanfile.py
@@ -39,8 +39,7 @@ class rsltestRecipe(ConanFile):
     def requirements(self):
         self.requires("libassert/2.1.5", transitive_headers=True, transitive_libs=True)
         self.requires("rsl-config/0.1", transitive_headers=False, transitive_libs=True)
-        self.requires("rsl-serialize/0.1", transitive_headers=False, transitive_libs=True)
-        self.requires("rsl-assert/0.1", transitive_headers=True, transitive_libs=True)
+        self.requires("rsl-xml/0.1", transitive_headers=False, transitive_libs=True)
 
     def layout(self):
         cmake_layout(self)
@@ -78,7 +77,7 @@ class rsltestRecipe(ConanFile):
         self.cpp_info.components["test_main"].set_property("cmake_target_name", "rsl::test_main")
         self.cpp_info.components["test_main"].includedirs = ["include"]
         self.cpp_info.components["test_main"].libdirs = ["lib"]
-        self.cpp_info.components["test_main"].requires = ["test", "rsl-config::config", "rsl-serialize::serialize", "rsl-assert::assert"] # depend on primary component
+        self.cpp_info.components["test_main"].requires = ["test", "rsl-config::config", "rsl-xml::xml"] # depend on primary component
         self.cpp_info.components["test_main"].libs = ["rsltest_main"]
 
 
