4b2dc9261d3dea4f7f5739e27f47f7959255c57a
Tsche <che@pydong.org>
2025-07-30 13:43:59 +0000

    refactor

--- include/rsl/testing/_testing_impl/paramset.hpp
+++ include/rsl/testing/_testing_impl/paramset.hpp
@@ -10,25 +10,32 @@ struct ParamSet {
 
   template <typename T>
     requires(!std::same_as<std::remove_cvref_t<T>, ParamSet>)
-  consteval explicit(std::invocable<T>) ParamSet(T&& v) {
+  consteval explicit(std::invocable<T>) ParamSet(T&& v) : value(make(std::forward<T>(v))) {
     // this constructor is only explicit if the argument is a nullary function
     // this is done so `initializer_list<ParamSet>` does not win over Params' templated generator
     // function ctor
     // TODO only do this if the return type is an iterable with element_type ParamSet or tuple-like
-    value = std::define_static_array(std::vector{std::meta::reflect_constant(v)});
   }
 
   template <typename... Ts>
     requires(sizeof...(Ts) > 1 && !(std::same_as<std::remove_cvref_t<Ts>, ParamSet> || ...))
-  consteval ParamSet(Ts&&... ts) {
+  consteval ParamSet(Ts&&... ts) : value(make(std::forward<Ts>(ts)...)) {}
+
+private:
+  template <typename... Ts>
+  static consteval rsl::span<std::meta::info const> make(Ts&&... ts) {
     std::vector<std::meta::info> elts;
     template for (auto&& elt : {std::forward<Ts>(ts)...}) {
-      if constexpr (std::same_as<std::remove_cvref_t<decltype(elt)>, std::meta::info>)
+      using type = std::remove_cvref_t<decltype(elt)>;
+      if constexpr (std::convertible_to<type, std::string_view>) {
+        elts.push_back(std::meta::reflect_constant_string(elt));
+      } else if constexpr (std::same_as<type, std::meta::info>) {
         elts.push_back(elt);
-      else
+      } else {
         elts.push_back(std::meta::reflect_constant(elt));
+      }
     }
-    value = std::define_static_array(elts);
+    return std::define_static_array(elts);
   }
 };
-}  // namespace rsl::testing
+}  // namespace rsl::testing::_testing_impl
